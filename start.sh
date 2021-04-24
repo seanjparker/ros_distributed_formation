a14dfd612b079de9de019e8ac394dec1c40debc0
Sean Parker <sean.parker067@gmail.com>
2021-04-24 16:17:20 +0100

    improve formation control

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ gnome-terminal -x sh -c "roslaunch mini_project multi_slam.launch; bash"
 gnome-terminal -x sh -c "python python/navigation.py --robot=tb3_0; bash"
 gnome-terminal -x sh -c "python python/navigation.py --robot=tb3_1; bash"
 gnome-terminal -x sh -c "python python/navigation.py --robot=tb3_2; bash"
+gnome-terminal -x sh -c "python python/navigation.py --robot=tb3_3; bash"
 # gnome-terminal -x sh -c "python python/navigation.py --robot=tb3_3; bash"
 # python python/navigation.py --robot=r_1 &
 # python python/navigation.py --robot=r_2 &
